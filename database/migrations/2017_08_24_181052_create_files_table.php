5f45d0877c7870154e44dab41667882ac917b2c9
Morgan Lane <morgan@morganlanewebdesign.co.uk>
2017-08-24 19:16:52 +0100

    Added files and folders migrations

---
+++ database/migrations/2017_08_24_181052_create_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('folder_id');
+            $table->string('file_name');
+            $table->string('file_extension');
+            $table->bigInteger('file_size');
+            $table->string('file_hash');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
