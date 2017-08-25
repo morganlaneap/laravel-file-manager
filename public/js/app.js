b077acb07c773dc48f152251c1cbd517c43a7982
Morgan Lane <morgan@morganlanewebdesign.co.uk>
2017-08-25 20:12:56 +0100

    Toastr notifications

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,16 @@
 /**
  * Created by Morgan Lane on 24/08/2017.
  */
-$('#upload-file-submit').click(function () {
-   $('#upload-dropzone').submit();
-});
-
-$(function (){
-    Dropzone.options.uploadDropzone = {
-        paramName: "files",
-        uploadMultiple:true,
-        maxFilesize:1,
-        autoProcessQueue: true,
-        addRemoveLinks: true,
-        acceptedFiles: ".png, .jpg, .jpeg",
-        dictFallbackMessage:"Your browser does not support drag'n'drop file uploads.",
-        dictRemoveFile: "Remove",
-        dictFileTooBig:"Image is bigger than 6MB",
-
-        accept: function(file, done) {
-            console.log("Uploaded");
-        },
-
-        init:function() {
-
-        },
-
-        success: function(file,done){
-            console.log("All files done!");
-        }
+function notify(message, type) {
+    switch (type) {
+        case 1:
+            toastr.success(message, 'Success', {timeOut: 5000})
+            break;
+        case 2:
+            toastr.info(message, 'Info', {timeOut: 5000})
+            break;
+        case 3:
+            toastr.error(message, 'Error', {timeOut: 5000})
+            break;
     }
-});
+}
