6c30d65e96c5e6aff9f8476a930a34c4a93e8133
Morgan Lane <morgan.lane@assistivepartner.co.uk>
2017-08-29 15:22:58 +0100

    Added loading directive to show when the explorer is updating

--- public/js/angular/fileManager.js
+++ public/js/angular/fileManager.js
@@ -128,3 +128,24 @@ fileManagerApp.controller('explorerController', function ($scope, $http, $rootSc
         });
     }
 });
+
+fileManagerApp.directive("fmLoading", function($http) {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+            scope.isLoading = function() {
+                return $http.pendingRequests.length > 0;
+            };
+
+            scope.$watch(scope.isLoading, function (v) {
+               if (v) {
+                   $("#" + attrs.fmLoading).hide();
+                   elm.show();
+               } else {
+                   $("#" + attrs.fmLoading).show();
+                   elm.hide();
+               }
+            });
+        }
+    }
+});
--- resources/views/shared/explorer.blade.php
+++ resources/views/shared/explorer.blade.php
@@ -3,44 +3,51 @@
         <div class="row">
             <span class="hidden" ng-bind="folder" id="current-folder"></span>
             <div class="col-md-12">
-                <span ng-show="folders.length+files.length==0 && folder==0">No files found.</span>
-                <table class="table table-hover table-responsive" ng-show="folder==0 ? folders.length + files.length>0 : true">
-                    <thead>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>Size</th>
-                        <th>Date Modified</th>
-                        <th>Actions</th>
-                    </thead>
-                    <tr class="clickable-row" ng-hide="folder==0" ng-click="getParentFolderId()">
-                        <td>
-                            <i class="fa fa-level-up fa-2x"></i>&nbsp;&nbsp;
-                        </td>
-                        <td></td><td></td><td></td><td></td>
-                    </tr>
-                    <tr class="clickable-row" ng-repeat="f in folders" ng-click="$parent.folder=f.id; getFiles()">
-                        <td><i class="fa fa-folder fa-2x" style="vertical-align:  -20%;"></i>&nbsp;&nbsp;&nbsp;&nbsp;@{{ f.folder_name }}</td>
-                        <td>Folder</td>
-                        <td></td>
-                        <td>@{{ f.updated_at }}</td>
-                        <td></td>
-                    </tr>
-                    <tr ng-repeat="file in files">
-                        <td><i class="fa fa-file-text-o fa-2x" style="vertical-align: -20%;"></i>&nbsp;&nbsp;&nbsp;&nbsp;@{{ file.file_name }}</td>
-                        <td>@{{ file.file_extension }}</td>
-                        <td>@{{ file.file_size/1024/1024|number:2 }} MB</td>
-                        <td>@{{ file.updated_at }}</td>
-                        <td>
-                            <a class="btn btn-default btn-sm" href="{{route('explorer.download')}}/@{{ file.id }}"><i class="fa fa-download"></i></a>
-                            <a class="btn btn-danger btn-sm" ng-click="deleteFile(file.id)"><i class="fa fa-trash"></i></a>
-                        </td>
-                    </tr>
-                </table>
+
+                <div fm-loading="explorer" class="text-center">
+                    <i class="fa fa-spin fa-refresh fa-3x"></i>
+                </div>
+
+                <div id="explorer">
+                    <span ng-show="folders.length+files.length==0 && folder==0">No files found.</span>
+                    <table class="table table-hover table-responsive" ng-show="folder==0 ? folders.length + files.length>0 : true">
+                        <thead>
+                            <th>Name</th>
+                            <th>Type</th>
+                            <th>Size</th>
+                            <th>Date Modified</th>
+                            <th>Actions</th>
+                        </thead>
+                        <tr class="clickable-row" ng-hide="folder==0" ng-click="getParentFolderId()">
+                            <td>
+                                <i class="fa fa-level-up fa-2x"></i>&nbsp;&nbsp;
+                            </td>
+                            <td></td><td></td><td></td><td></td>
+                        </tr>
+                        <tr class="clickable-row" ng-repeat="f in folders" ng-click="$parent.folder=f.id; getFiles()">
+                            <td><i class="fa fa-folder fa-2x" style="vertical-align:  -20%;"></i>&nbsp;&nbsp;&nbsp;&nbsp;@{{ f.folder_name }}</td>
+                            <td>Folder</td>
+                            <td></td>
+                            <td>@{{ f.updated_at }}</td>
+                            <td></td>
+                        </tr>
+                        <tr ng-repeat="file in files">
+                            <td><i class="fa fa-file-text-o fa-2x" style="vertical-align: -20%;"></i>&nbsp;&nbsp;&nbsp;&nbsp;@{{ file.file_name }}</td>
+                            <td>@{{ file.file_extension }}</td>
+                            <td>@{{ file.file_size/1024/1024|number:2 }} MB</td>
+                            <td>@{{ file.updated_at }}</td>
+                            <td>
+                                <a class="btn btn-default btn-sm" href="{{route('explorer.download')}}/@{{ file.id }}"><i class="fa fa-download"></i></a>
+                                <a class="btn btn-danger btn-sm" ng-click="deleteFile(file.id)"><i class="fa fa-trash"></i></a>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
+<br>
 <!-- add folder modal -->
 <div ng-controller="explorerController" class="modal fade" id="folder-create-modal" tabindex="-1" role="dialog">
     <div class="modal-dialog modal-sm" role="document">
