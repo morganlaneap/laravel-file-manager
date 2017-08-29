6189ef556d0eb3b07e7bd736135653917f4b4dc9
Morgan Lane <morgan.lane@assistivepartner.co.uk>
2017-08-29 15:28:51 +0100

    Removed broken profile code (oops!)

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,24 +10,11 @@
     </div>
   </div>
 
-  <div ng-app="profile" ng-controller="profileController">
+  <div>
     <div class="container">
       <div class="row">
         <div class="col-md-4">
-          <form name="profileForm">
-            <div class="form-group">
-              <input type="text" class="form-control @{{textClass}}" ng-model="textClass" />
-            </div>
-
-            <input name="userEmail" type="email" class="form-control" ng-model="email" />
-            <span class="text-danger" ng-show="profileForm.userEmail.$error.email">Not a valid email!</span>
-
-            <div ng-repeat="x in names | orderBy:'number'">
-                @{{ x.number }}<br/>
-            </div>
-
-            <input name="userFirstName" type="text" class="form-control" ng-model="firstName | uppercase" />
-        </form>
+          
         </div>
       </div>
     </div>
