e5bd7890fdfd08f12a47aee4be6c436fea560556
Morgan Lane <morgan.lane@assistivepartner.co.uk>
2017-08-30 16:58:07 +0100

    Fixed config seeds

--- database/seeds/ConfigsSeeder.php
+++ database/seeds/ConfigsSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use App\Config;
 class ConfigsSeeder extends Seeder
 {
     /**
@@ -11,11 +12,32 @@ class ConfigsSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('configs')->insert([
+        Config::firstOrNew(array(
+            'item' => 'site_name',
+            'value' => 'LaravelFileManager',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ))->save();
+
+        Config::firstOrNew(array(
+            'item' => 'show_footer_message',
+            'value' => 'true',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ))->save();
+
+        /*DB::table('configs')->firstOrNew([
             'item' => 'site_name',
             'value' => 'LaravelFileManager',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
+
+        DB::table('configs')->firstOrNew([
+            'item' => 'show_footer_message',
+            'value' => 'true',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);*/
     }
 }
