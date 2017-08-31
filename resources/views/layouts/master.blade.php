5fb2d4c0d4207f5e756c8e2c03372b523a2accb8
morganlaneap <morgan.lane@outlook.com>
2017-08-31 16:46:21 +0100

    Moved urls to a new shared view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,19 +16,7 @@
     <link href="{{ asset('css/toastr.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <script>
-        // urls
-        var upload_url = '{{route('file.upload')}}';
-        var explorer_files_url = '{{route('explorer.files')}}';
-        var explorer_folders_url = '{{route('explorer.folders')}}';
-        var explorer_parent_url = '{{route('explorer.folder.parent')}}';
-        var explorer_delete_url = '{{route('explorer.delete')}}';
-        var explorer_create_url = '{{route('explorer.folder.create')}}';
-        var explorer_rename_file_url = '{{route('explorer.file.rename')}}';
-        var explorer_rename_folder_url = '{{route('explorer.folder.rename')}}';
-        var explorer_move_file_url = '{{route('explorer.file.move')}}';
-        var explorer_get_folder_name_url = '{{route('explorer.folder.getName')}}';
-    </script>
+    @include('shared.urls')
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
---
+++ resources/views/shared/urls.blade.php
@@ -0,0 +1,13 @@
+<script>
+    // urls
+    var upload_url = '{{route('file.upload')}}';
+    var explorer_files_url = '{{route('explorer.files')}}';
+    var explorer_folders_url = '{{route('explorer.folders')}}';
+    var explorer_parent_url = '{{route('explorer.folder.parent')}}';
+    var explorer_delete_url = '{{route('explorer.delete')}}';
+    var explorer_create_url = '{{route('explorer.folder.create')}}';
+    var explorer_rename_file_url = '{{route('explorer.file.rename')}}';
+    var explorer_rename_folder_url = '{{route('explorer.folder.rename')}}';
+    var explorer_move_file_url = '{{route('explorer.file.move')}}';
+    var explorer_get_folder_name_url = '{{route('explorer.folder.getName')}}';
+</script>
