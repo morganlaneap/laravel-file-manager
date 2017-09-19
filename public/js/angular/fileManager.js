4a143d5582808be8e3b2dae6a2331c68c6a7cff5
Morgan Lane <morgan.lane@outlook.com>
2017-09-19 20:05:44 +0100

    Beautified the angular file

--- public/js/angular/fileManager.js
+++ public/js/angular/fileManager.js
@@ -2,327 +2,326 @@ var fileManagerApp = angular.module('fileManager', ['angularFileUpload', 'ui.sor
 
 // Controllers //
 fileManagerApp.controller('uploadFileController', ['$scope', 'FileUploader', '$rootScope', function($scope, FileUploader, $rootScope) {
-    var uploader = $scope.uploader = new FileUploader({
-        url: upload_url,
-        formData: [{
-            folder: $('#current-folder').html(),
-            '_token' : $('meta[name=csrf-token]').attr("content")
-        }],
-        method: 'post'
-    });
+  var uploader = $scope.uploader = new FileUploader({
+    url: upload_url,
+    formData: [{
+      folder: $('#current-folder').html(),
+      '_token': $('meta[name=csrf-token]').attr("content")
+    }],
+    method: 'post'
+  });
 
-    // an async filter
-    uploader.filters.push({
-        name: 'asyncFilter',
-        fn: function(item /*{File|FileLikeObject}*/, options, deferred) {
-            setTimeout(deferred.resolve, 1e3);
-        }
-    });
+  // an async filter
+  uploader.filters.push({
+    name: 'asyncFilter',
+    fn: function(item /*{File|FileLikeObject}*/ , options, deferred) {
+      setTimeout(deferred.resolve, 1e3);
+    }
+  });
 
-    uploader.onBeforeUploadItem = function(item) {
-        item.formData = [{
-            folder: $('#current-folder').html(),
-            '_token' : $('meta[name=csrf-token]').attr("content")
-        }];
-    };
+  uploader.onBeforeUploadItem = function(item) {
+    item.formData = [{
+      folder: $('#current-folder').html(),
+      '_token': $('meta[name=csrf-token]').attr("content")
+    }];
+  };
 
-    uploader.onSuccessItem = function(fileItem, response, status, headers) {
-        notify('File uploaded.', 1);
-        $scope.getFiles();
-    };
-    uploader.onErrorItem = function(fileItem, response, status, headers) {
-        notify('Failed to upload file: ' + status, 3);
-    };
+  uploader.onSuccessItem = function(fileItem, response, status, headers) {
+    notify('File uploaded.', 1);
+    $scope.getFiles();
+  };
+  uploader.onErrorItem = function(fileItem, response, status, headers) {
+    notify('Failed to upload file: ' + status, 3);
+  };
 
-    $scope.getFiles = function() {
-        $rootScope.$emit('getFiles', {});
-    };
+  $scope.getFiles = function() {
+    $rootScope.$emit('getFiles', {});
+  };
 }]);
 
-fileManagerApp.controller('explorerController', function ($scope, $http, $rootScope) {
-    $scope.folder = 0;
-    $scope.userId = user_id;
-    $scope.userUsagePercentage = 0;
+fileManagerApp.controller('explorerController', function($scope, $http, $rootScope) {
+  $scope.folder = 0;
+  $scope.userId = user_id;
+  $scope.userUsagePercentage = 0;
 
-    $rootScope.$on('getFiles', function() {
-        $scope.getFiles();
+  $rootScope.$on('getFiles', function() {
+    $scope.getFiles();
+  });
+
+  $scope.getFiles = function() {
+
+    var folder = $scope.folder;
+
+    var data = {
+      folder: folder,
+      '_token': $('meta[name=csrf-token]').attr("content")
+    };
+
+    $http({
+      method: 'POST',
+      url: explorer_files_url,
+      data: data
+    }).then(function(response) {
+      $scope.files = response.data;
     });
 
-    $scope.getFiles = function() {
+    $http({
+      method: 'POST',
+      url: explorer_folders_url,
+      data: data
+    }).then(function(response) {
+      $scope.folders = response.data;
+    });
 
-        var folder = $scope.folder;
+    $http({
+      method: 'POST',
+      url: explorer_get_folder_bc_url,
+      data: data
+    }).then(function(response) {
+      $scope.folderBreadcrumb = response.data;
+    });
 
-        var data = {
-            folder: folder,
-            '_token' : $('meta[name=csrf-token]').attr("content")
-        };
+    //$scope.getFolderBreadcrumb();
+  };
 
-        $http({
-            method: 'POST',
-            url: explorer_files_url,
-            data: data
-        }).then(function(response) {
-            $scope.files = response.data;
-        });
+  $scope.getParentFolderId = function() {
 
-        $http({
-            method: 'POST',
-            url: explorer_folders_url,
-            data: data
-        }).then(function(response) {
-            $scope.folders = response.data;
-        });
-
-        $http({
-            method: 'POST',
-            url: explorer_get_folder_bc_url,
-            data: data
-        }).then(function(response) {
-            $scope.folderBreadcrumb = response.data;
-        });
-
-        //$scope.getFolderBreadcrumb();
+    var data = {
+      folder: $scope.folder,
+      '_token': $('meta[name=csrf-token]').attr("content")
     };
 
-    $scope.getParentFolderId = function() {
-
-        var data = {
-            folder: $scope.folder,
-            '_token' : $('meta[name=csrf-token]').attr("content")
-        };
-
-        $http({
-            method: 'POST',
-            url: explorer_parent_url,
-            data: data
-        }).then(function(response) {
-            $scope.folder = response.data["parent_folder"];
-            console.log(response.data["parent_folder"]);
-            $scope.getFiles();
-        });
+    $http({
+      method: 'POST',
+      url: explorer_parent_url,
+      data: data
+    }).then(function(response) {
+      $scope.folder = response.data["parent_folder"];
+      console.log(response.data["parent_folder"]);
+      $scope.getFiles();
+    });
 
 
+  };
+
+  $scope.deleteFile = function(id) {
+    var url = explorer_delete_url;
+
+    url = url + "/" + id;
+
+    $http.get(url).then(function(response) {
+      if (response.status == "200") {
+        notify(response.msg, 1);
+      } else {
+        notify(response.msg, 3);
+      }
+
+      $scope.getFiles();
+    });
+  };
+
+  $scope.createFolder = function() {
+    var data = {
+      name: $scope.folder_name,
+      description: $scope.folder_desc,
+      parent: $scope.folder,
+      '_token': $('meta[name=csrf-token]').attr("content")
     };
 
-    $scope.deleteFile = function(id) {
-        var url = explorer_delete_url;
+    $http({
+      method: 'POST',
+      url: explorer_create_url,
+      data: data
+    }).then(function(response) {
+      notify(response.data, 1);
+      $('#folder-create-modal').modal('hide');
+      $scope.getFiles();
 
-        url = url + "/" + id;
+      $scope.folder_name = '';
+      $scope.folder_desc = '';
+    });
+  };
 
-        $http.get(url).then(function (response) {
-            if (response.status == "200") {
-                notify(response.msg, 1);
-            } else
-            {
-                notify(response.msg, 3);
-            }
-
-            $scope.getFiles();
-        });
+  $scope.renameFile = function() {
+    var data = {
+      fileName: $scope.fileName,
+      fileId: $scope.fileId,
+      '_token': $('meta[name=csrf-token]').attr("content")
     };
 
-    $scope.createFolder = function() {
-        var data = {
-            name: $scope.folder_name,
-            description: $scope.folder_desc,
-            parent: $scope.folder,
-            '_token' : $('meta[name=csrf-token]').attr("content")
-        };
+    $http({
+      method: 'POST',
+      url: explorer_rename_file_url,
+      data: data
+    }).then(function(response) {
+      notify(response.data, 1);
+      $('#rename-file-modal').modal('hide');
+      $scope.getFiles();
+    });
+  };
 
-        $http({
-            method: 'POST',
-            url: explorer_create_url,
-            data: data
-        }).then(function(response) {
-            notify(response.data, 1);
-            $('#folder-create-modal').modal('hide');
-            $scope.getFiles();
-
-            $scope.folder_name = '';
-            $scope.folder_desc = '';
-        });
+  $scope.renameFolder = function() {
+    var data = {
+      id: $scope.folderId,
+      name: $scope.folderName,
+      '_token': $('meta[name=csrf-token]').attr('content')
     };
 
-    $scope.renameFile = function() {
-        var data = {
-            fileName: $scope.fileName,
-            fileId: $scope.fileId,
-            '_token' : $('meta[name=csrf-token]').attr("content")
-        };
+    $http({
+      method: 'POST',
+      url: explorer_rename_folder_url,
+      data: data
+    }).then(function(response) {
+      notify(response.data, 1);
+      $('#rename-folder-modal').modal('hide');
+      $scope.getFiles();
+    });
+  };
 
-        $http({
-            method: 'POST',
-            url: explorer_rename_file_url,
-            data: data
-        }).then(function(response) {
-            notify(response.data, 1);
-            $('#rename-file-modal').modal('hide');
-            $scope.getFiles();
-        });
+  $scope.orderByMe = function(x) {
+    $scope.myOrderBy = x;
+  };
+
+  $scope.showFolderRenameModal = function($event) {
+    $event.stopPropagation();
+
+    $('#rename-folder-modal').modal('toggle');
+  };
+
+  $scope.moveFile = function(folder, file) {
+    var data = {
+      folderId: folder,
+      fileId: file,
+      '_token': $('meta[name=csrf-token]').attr('content')
     };
 
-    $scope.renameFolder = function() {
-        var data = {
-            id: $scope.folderId,
-            name: $scope.folderName,
-            '_token': $('meta[name=csrf-token]').attr('content')
-        };
+    $http({
+      method: 'POST',
+      url: explorer_move_file_url,
+      data: data
+    }).then(function(response) {
+      notify('File moved.', 1);
+      $scope.getFiles();
+    });
 
-        $http({
-            method: 'POST',
-            url: explorer_rename_folder_url,
-            data: data
-        }).then(function(response) {
-            notify(response.data, 1);
-            $('#rename-folder-modal').modal('hide');
-            $scope.getFiles();
-        });
+    //$scope.getFiles();
+  };
+
+  $scope.dropzoneFiles = [];
+
+  $scope.draggable = {
+    connectWith: ".dropzone",
+    start: function(e, ui) {
+      $(ui.item).addClass('dragging-row');
+    },
+    update: function(e, ui) {
+      if (ui.item.sortable.droptarget[0].classList[0] !== "dropzone")
+        ui.item.sortable.cancel();
+    },
+    stop: function(e, ui) {
+      $(ui.item).removeClass('dragging-row');
+      if (ui.item.sortable.droptarget[0].classList[0] == "dropzone") {
+        $scope.$apply($scope.dragging = false);
+
+        // Now move the file
+        var folderId = $(ui.item.sortable.droptarget[0]).data('folder');
+        var fileId = $(ui.item).data('file');
+
+        $scope.moveFile(folderId, fileId);
+
+        $scope.dropzone = {}; // clear the dropzone
+        $scope.dropzoneFiles = [];
+      }
+    }
+  };
+
+  $scope.dropzone = {};
+
+  $scope.getQuota = function() {
+    var data = {
+      userId: $scope.userId,
+      '_token': $('meta[name=csrf-token]').attr("content")
     };
 
-    $scope.orderByMe = function(x) {
-        $scope.myOrderBy = x;
-    };
+    $http({
+      method: 'POST',
+      url: explorer_get_quota_url,
+      data: data
+    }).then(function(response) {
+      $scope.userQuota = response.data[0]["disk_quota"];
+      $scope.userUsage = response.data[0]["disk_usage"];
 
-    $scope.showFolderRenameModal = function($event) {
-        $event.stopPropagation();
+      $scope.userUsagePercentage = (($scope.userUsage / $scope.userQuota) * 100).toFixed(1);
 
-        $('#rename-folder-modal').modal('toggle');
-    };
+      $('.user-quota-circle').attr('class', 'c100 user-quota-circle p' + Math.round($scope.userUsagePercentage));
+    });
+  };
 
-    $scope.moveFile = function(folder, file) {
-        var data = {
-            folderId: folder,
-            fileId: file,
-            '_token': $('meta[name=csrf-token]').attr('content')
-        };
-
-        $http({
-            method: 'POST',
-            url: explorer_move_file_url,
-            data: data
-        }).then(function(response) {
-            notify('File moved.', 1);
-            $scope.getFiles();
-        });
-
-        //$scope.getFiles();
-    };
-
-    $scope.dropzoneFiles = [];
-
-    $scope.draggable = {
-        connectWith: ".dropzone",
-        start: function (e, ui) {
-            $(ui.item).addClass('dragging-row');
-        },
-        update: function (e, ui) {
-            if (ui.item.sortable.droptarget[0].classList[0] !== "dropzone")
-                ui.item.sortable.cancel();
-        },
-        stop: function (e, ui) {
-            $(ui.item).removeClass('dragging-row');
-            if (ui.item.sortable.droptarget[0].classList[0] == "dropzone") {
-                $scope.$apply($scope.dragging = false);
-
-                // Now move the file
-                var folderId = $(ui.item.sortable.droptarget[0]).data('folder');
-                var fileId = $(ui.item).data('file');
-
-                $scope.moveFile(folderId, fileId);
-
-                $scope.dropzone = {}; // clear the dropzone
-                $scope.dropzoneFiles = [];
-            }
-        }
-    };
-
-    $scope.dropzone = {};
-
-    $scope.getQuota = function() {
-        var data = {
-            userId: $scope.userId,
-            '_token' : $('meta[name=csrf-token]').attr("content")
-        };
-
-        $http({
-            method: 'POST',
-            url: explorer_get_quota_url,
-            data: data
-        }).then(function(response) {
-            $scope.userQuota = response.data[0]["disk_quota"];
-            $scope.userUsage = response.data[0]["disk_usage"];
-
-            $scope.userUsagePercentage = (($scope.userUsage / $scope.userQuota) * 100).toFixed(1);
-
-            $('.user-quota-circle').attr('class', 'c100 user-quota-circle p' + Math.round($scope.userUsagePercentage));
-        });
-    };
-
-    $scope.getFiles();
-    $scope.getQuota();
+  $scope.getFiles();
+  $scope.getQuota();
 });
 
-fileManagerApp.controller('settingsController', function($scope){
-    $scope.checkShowFooterBox = function() {
-        $scope.showFooterTextbox = $('#showFooter').is(':checked');
-    };
+fileManagerApp.controller('settingsController', function($scope) {
+  $scope.checkShowFooterBox = function() {
+    $scope.showFooterTextbox = $('#showFooter').is(':checked');
+  };
 });
 
-fileManagerApp.controller('userManagementController', function($scope, $http){
-    $scope.userSearch = '';
+fileManagerApp.controller('userManagementController', function($scope, $http) {
+  $scope.userSearch = '';
 
-    $scope.getUsers = function() {
-        var data = {
-            searchString: $scope.userSearch,
-            '_token' : $('meta[name=csrf-token]').attr("content")
-        };
-
-        $http({
-            method: 'POST',
-            url: admin_users_get_url,
-            data: data
-        }).then(function(response) {
-            $scope.userResults = response.data;
-        });
+  $scope.getUsers = function() {
+    var data = {
+      searchString: $scope.userSearch,
+      '_token': $('meta[name=csrf-token]').attr("content")
     };
 
-    $scope.saveUserDetails = function() {
-        var data = {
-            userId: $scope.userId,
-            userQuota: $scope.userQuota,
-            '_token' : $('meta[name=csrf-token]').attr("content")
-        };
+    $http({
+      method: 'POST',
+      url: admin_users_get_url,
+      data: data
+    }).then(function(response) {
+      $scope.userResults = response.data;
+    });
+  };
 
-        $http({
-            method: 'POST',
-            url: admin_users_save_url,
-            data: data
-        }).then(function(response) {
-            notify("User saved.", 1);
-        });
+  $scope.saveUserDetails = function() {
+    var data = {
+      userId: $scope.userId,
+      userQuota: $scope.userQuota,
+      '_token': $('meta[name=csrf-token]').attr("content")
     };
+
+    $http({
+      method: 'POST',
+      url: admin_users_save_url,
+      data: data
+    }).then(function(response) {
+      notify("User saved.", 1);
+    });
+  };
 });
 
 // Directives //
 fileManagerApp.directive("fmLoading", function($http) {
-    return {
-        restrict: 'A',
-        link: function(scope, elm, attrs) {
-            scope.isLoading = function() {
-                return $http.pendingRequests.length > 0;
-            };
+  return {
+    restrict: 'A',
+    link: function(scope, elm, attrs) {
+      scope.isLoading = function() {
+        return $http.pendingRequests.length > 0;
+      };
 
-            scope.$watch(scope.isLoading, function (v) {
-                if (v) {
-                    $("#" + attrs.fmLoading).hide();
-                    elm.show();
-                } else {
-                    $("#" + attrs.fmLoading).show();
-                    elm.hide();
-                }
-            });
+      scope.$watch(scope.isLoading, function(v) {
+        if (v) {
+          $("#" + attrs.fmLoading).hide();
+          elm.show();
+        } else {
+          $("#" + attrs.fmLoading).show();
+          elm.hide();
         }
+      });
     }
+  }
 });
 
 //// OLD CODE - DOESN'T WORK
